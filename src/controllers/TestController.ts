5539680c15c3b0f574ad2b2dd01571e3bb3ca6ef
yuyoungsu5582 <yuyoungsu5582@gmail.com>
2023-02-12 16:11:36 +0900

    [Feat] AWS s3 기능 연동

--- src/controllers/TestController.ts
+++ src/controllers/TestController.ts
@@ -2,12 +2,20 @@ import {Service} from 'typedi';
 import {JsonController,
     HttpCode,
     Get,
+    Req,
     QueryParam,
+    Post,
+    UseBefore,
+    UploadedFile,
+    Param
     } from "routing-controllers";
 import {OpenAPI} from "routing-controllers-openapi";
 import { Request, Response } from 'express';
 import { TestService } from '../services/TestService';
 import { TestDto } from '../dtos/TestDto';
+import { logger } from '../utils/Logger';
+import { getFile, uploadFile } from '../loaders/s3';
+
 
 @JsonController('/test')
 @Service()
@@ -18,8 +26,33 @@ export class AuthController{
     @OpenAPI({
         summary:"Test Function"
     })
-    public async test(@QueryParam("test")test:TestDto){
+    public async test(@QueryParam("test")test:TestDto, @Req() req:Request){
+        console.log('22');
+        console.log(test);
         const result = await this.testService.testFunction(test);
+        console.log(result);
+        return result;
+    }
+
+    @Post("/image")
+    @OpenAPI({
+        summary:"Test Image Function"
+    })
+    
+    public async testImage(@UploadedFile('image')file : Express.Multer.File, @Req() req:Request){
+        
+        const result = await uploadFile(file.originalname,file.buffer);
+        
+        return 'Complete';
+    }
+
+    @Get("/id")
+    @OpenAPI({
+        summary:"Test Get Image Function"
+    })
+    public async getImage(@QueryParam('path')path : string){
+        
+        const result = await getFile(path);
         return result;
     }
 }
