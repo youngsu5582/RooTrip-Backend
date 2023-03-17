36428ce0995bdb731272525e2f87f736d33b151b
youngsu5582 <yuyoungsu5582@gmail.com>
2023-03-17 13:57:06 +0900

    [ETC] 코드 리뷰용 커밋

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -11,5 +11,146 @@ const key = env.key;
 
 @Service()
 export class AuthService{
+    private userRepository :typeof UserRepository
+    constructor(){
+        this.userRepository = UserRepository;
+    };
+    public async register(createUserDto : CreateLocalUserDto){
+        let result:ResponseType;
+        const email = createUserDto.email;
+        if(await this.userRepository.getByEmail(email)){
+            result = {
+                status:false,
+                message:'이메일이 중복입니다.'
+            }
+            return result;
+        }
+        const entity = createUserDto.toEntity();
+        const user = await this.userRepository.save(entity);
+        if(user)
+            result = {status:true,message:'회원가입 성공'};
+        else    
+            result = {status:false,message:'회원가입에 실패했습니다.'};
+        return result;
+    }
+    public async localLogin(loginUserDto : LoginUserDto){
+        const {email,password} = loginUserDto;  
+        const user = await this.userRepository.findOne({where:{email}});
+        
+        let result:ResponseType;
+        if(user){
+            if(await user.comparePassword(password))
+                result = {status:true,user};
+            else
+                result =  {status:false,message:'비밀번호가 일치하지 않습니다.'};
+        }
+        else{
+            result =  {status:false,message:'해당 이메일이 없습니다.'}
+        }
+        return result;
+    }
+    public async validateUserToken(id : string,refreshToken : string){
+        return await this.userRepository.findOne({where:{id,refreshToken}});
+    }
+    public async saveRefreshToken(id:string,refreshToken : string){
+        return await this.userRepository.update({id},{refreshToken});
+    }
+    public async checkDuplicateEmail(email:string){
+        return Boolean(!await this.userRepository.getByEmail(email));
+    }
+    public async checkDuplicateNickname(nickname:string){
+        return Boolean(!await this.userRepository.findOne({where:{nickname:nickname}}));
+    }
+
+    public async kakaoLogin(code:string) {
+        
+        const accessToken = await axios.post('https://kauth.kakao.com/oauth/token', {}, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            params:{
+                grant_type: 'authorization_code',
+                client_id: env.key.kakaoRestApi,
+                code,
+                redirect_uri : env.key.kakaoRedirectUri
+            }
+        })
+        .then(res=>res.data.access_token)
+        .catch(()=>(null));
+        const userInfo = await axios.post('https://kapi.kakao.com/v2/user/me',{},{
+            headers: {  
+                "Content-Type" : "application/x-www-form-urlencoded;charset",
+                "Authorization" : 'Bearer ' + accessToken
+            }
+        })
+        .then(res=>res.data)
+        .catch(()=>(null));
+        const result : KakaoUserDto = {
+            id: userInfo.id,
+            name: userInfo.properties.nickname,
+            toEntity : KakaoUserDto.prototype.toEntity,
+        }
+        
+        return result;
+    }
+    
+    public async naverLogin(code:string) {
+        const naverTokenUrl = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${key.NaverClientId}&client_secret=${key.NaverClientSecret}&code=${code}&state=state`;
+        const accessToken = await axios.post(naverTokenUrl,{},{})
+        .then(res=>res.data.access_token)
+        .catch(()=>(null));
+        
+        const userInfo:any = await axios.get('https://openapi.naver.com/v1/nid/me',{
+            headers:{
+                "Authorization" : `Bearer ${accessToken}`
+            }
+        }).then(res=>(res.data.response))
+        .catch(()=>(null));
+        const id = encrypt(userInfo.id);
+        
+        const result :NaverUserDto = {
+            id,
+            name : userInfo.name,
+            gender : userInfo.gender,
+            email: 'n_'+userInfo.email,
+            toEntity : NaverUserDto.prototype.toEntity,
+        }
+        
+        return result;
+    }
+    public async googleLogin(code:string){
+        let userInfo:any;
+        const result : GoogleUserDto = {
+            
+            id:userInfo.id ,
+            name : userInfo.name,
+            toEntity : GoogleUserDto.prototype.toEntity,
+        }
+        return result;
+    }
+    public async getUserById(id:string){
+        return await this.userRepository.getById(id);
+    }
+    public async socialRegister(createUserDto : SocialLoginType){
+        const entity = createUserDto.toEntity();
+        
+        const user = await this.userRepository.save(entity);
+        let result:ResponseType ; 
+        if(user)
+            result = {status:true,user};
+        else    
+            result = {status:false,message:'회원가입에 실패했습니다.'};
+        return result;
+    }
+    public async logout(jwtPayload : CustomJwtPayload,token:string){
+        const expiresIn = jwtPayload.exp - jwtPayload.iat;
+        const result = await addBlacklist(token,expiresIn);
+        if(result)
+            return result;
+        
+        return await this.userRepository.deleteRefreshTokenById(jwtPayload.userId);
+        
+    }
+
 
 }
