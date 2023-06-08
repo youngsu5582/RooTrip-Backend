af6577f86d4f1d3a325df92f5e781aa912b106db
WooJJam <jaemin5548@naver.com>
2023-06-08 23:10:13 +0900

    [Refactoring] Follow 관련 에러 부분 처리

---
+++ src/errors/user-error.ts
@@ -0,0 +1,31 @@
+import { ERROR } from ".";
+
+export interface NOT_EXISTED_USER extends ERROR {
+    status: false;
+    message: "존재하지 않는 회원입니다.";
+}
+
+export interface ALREADY_FOLLOWING extends ERROR {
+    status: false;
+    message: "이미 팔로잉 중 입니다."
+}
+
+export interface UNFOLLOW_FAILED extends ERROR {
+    status: false;
+    message: "이미 언팔로우 하였습니다.";
+  }
+
+export interface FOLLOW_FAILED extends ERROR {
+    status: false;
+    message: "팔로잉에 실패했습니다.";
+}
+
+export interface NOT_FOLLOWING extends ERROR {
+    status: false;
+    message: "팔로잉중이 아닙니다.";
+}
+
+export interface SELECT_FOLLOWING_LIST_FAILED extends ERROR {
+    status: false;
+    message: "팔로잉 목록 조회에 실패했습니다."
+}
